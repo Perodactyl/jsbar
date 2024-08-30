72f16964d0a1cd2b286382ab6dfce431a3438c6c
Perodactyl <friedwhoman@gmail.com>
2024-08-30 15:50:53 -0400

    Added logs for missing nodejs or bun

--- status.sh
+++ status.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 if [ -r "$JSBAR_HOME/jsbar.js" ]; then
+	if ! [ -x "$(command -v node)" ]; then
+		echo "Attempting to run JSBar with nodejs, but node is missing." >> "$JSBAR_HOME/status.log"
+		exit
+	fi
 	echo "Starting JSbar (njs)..." >> "$JSBAR_HOME/status.log"
 	echo "If you meant to start the indev version, delete jsbar.js" >> "$JSBAR_HOME/status.log"
 	node "$JSBAR_HOME/jsbar.js" status 2>> "$JSBAR_HOME/status.log"
 else
+	if ! [ -x "$(command -v bun)" ]; then
+		echo "Attempting to run JSBar with bun, but bun is missing." >> "$JSBAR_HOME/status.log"
+		exit
+	fi
 	echo "Starting JSbar (bun indev)..." >> "$JSBAR_HOME/status.log"
 	bun "$JSBAR_HOME/src/entry.ts" status 2>> "$JSBAR_HOME/status.log"
 fi
